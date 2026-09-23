695f508b366ea7c99ef764ef9323db371ae6fd92
ADITYAOG1 <adityakeshwani542@gmail.com>
2026-09-23 11:10:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const http = require("http");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages
+  ]
+});
+
+// Small web server for Render
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Bot is online!");
+}).listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
 });
 
 client.once("ready", () => {
